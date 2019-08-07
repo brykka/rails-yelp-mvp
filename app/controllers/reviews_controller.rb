a388925ffc185483d7924929e2c1292d23373890
Bryenne Kay <bryenne.k@gmail.com>
2019-08-07 15:21:44 +0900

    woooo

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@review)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
+  end
+end
